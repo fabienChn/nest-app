c2da3ea70587a95c1c534469d15e8f01d26cc6d4
Fabien Cohen <fabiencohen96@gmail.com>
2023-02-14 19:38:04 +0100

    setup wip

---
+++ prisma/migrations/20230214145955_init/migration.sql
@@ -0,0 +1,30 @@
+-- CreateTable
+CREATE TABLE "User" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+    "name" TEXT,
+
+    CONSTRAINT "User_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" SERIAL NOT NULL,
+    "createdAt" TIMESTAMP(3) NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updatedAt" TIMESTAMP(3) NOT NULL,
+    "text" TEXT NOT NULL,
+    "isLiked" BOOLEAN NOT NULL DEFAULT false,
+    "emitterId" INTEGER NOT NULL,
+    "receiverId" INTEGER NOT NULL,
+
+    CONSTRAINT "Message_pkey" PRIMARY KEY ("id")
+);
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_emitterId_fkey" FOREIGN KEY ("emitterId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
+
+-- AddForeignKey
+ALTER TABLE "Message" ADD CONSTRAINT "Message_receiverId_fkey" FOREIGN KEY ("receiverId") REFERENCES "User"("id") ON DELETE RESTRICT ON UPDATE CASCADE;
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,12 +1,78 @@
-import { Injectable } from '@nestjs/common';
+import {
+  ForbiddenException,
+  Injectable,
+} from '@nestjs/common';
+import { ConfigService } from '@nestjs/config';
+import { JwtService } from '@nestjs/jwt';
+import { PrismaClientKnownRequestError } from '@prisma/client/runtime';
+import * as argon from 'argon2';
+import { PrismaService } from 'src/prisma/prisma.service';
+import { AuthDto } from './dto';
 
 @Injectable()
 export class AuthService {
-  signup() {
-    return 'signing up';
+  constructor(
+    private prisma: PrismaService,
+    private jwt: JwtService,
+    private config: ConfigService,
+  ) {}
+
+  async signup(dto: AuthDto): Promise<string> {
+    const hash = await argon.hash(dto.password);
+
+    try {
+      const user = await this.prisma.user.create({
+        data: {
+          email: dto.email,
+          password: hash,
+        },
+      });
+
+      return this.signToken(user.id, user.email);
+    } catch (e) {
+      if (e instanceof PrismaClientKnownRequestError) {
+        if (e.code === 'P2002') {
+          throw new ForbiddenException(
+            'Email already exists',
+          );
+        }
+      }
+      throw e;
+    }
   }
 
-  signin() {
-    return 'signing in';
+  async signin(dto: AuthDto): Promise<string> {
+    const user = await this.prisma.user.findUnique({
+      where: {
+        email: dto.email,
+      },
+    });
+
+    if (!user) {
+      throw new ForbiddenException('Wrong credentials');
+    }
+
+    const isPasswordMatching = await argon.verify(
+      user.password,
+      dto.password,
+    );
+
+    if (!isPasswordMatching) {
+      throw new ForbiddenException('Wrong credentials');
+    }
+
+    return this.signToken(user.id, user.email);
+  }
+
+  async signToken(
+    userId: number,
+    email: string,
+  ): Promise<string> {
+    const payload = { sub: userId, email };
+
+    return this.jwt.signAsync(payload, {
+      expiresIn: '60m',
+      secret: this.config.get('JWT_SECRET'),
+    });
   }
 }
